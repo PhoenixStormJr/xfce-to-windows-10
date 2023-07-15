79dc886ed5c923f30a744326f0d8f6ea677d407d
PhoenixStormJr <66498788+PhoenixStormJr@users.noreply.github.com>
2023-07-15 00:23:14 +0000

    Update ScriptxfceToWindows10.sh

--- ScriptxfceToWindows10.sh
+++ ScriptxfceToWindows10.sh
@@ -9,3 +9,29 @@ sudo chmod 555 -R /usr/share/themes/Kali-Windows-10-theme
 echo "applying the theme to your computer"
 xfconf-query -c xsettings -p /Net/ThemeName -s "Kali-Windows-10-theme"
 echo "All commands finished successfully, your computer should now look like windows 10."
+echo "downloading windows 10 icon theme from b00merang"
+sudo apt update
+sudo apt install git -y
+git clone https://github.com/B00merang-Artwork/Windows-10.git
+echo "changing name of the icon folder to Windows-10-Icons"
+mv Windows-10 Windows-10-Icons
+echo "Moving Windows-10-Icons to /usr/share/icons"
+sudo mv Windows-10-Icons /usr/share/icons
+echo "untarring kali windows 10 icons"
+tar -xvf setupStuff/Kali-Windows-10-Icons.tar
+echo "copying missing icons to /usr/share/icons/Windows-10-Icons"
+sudo cp -r Kali-Windows-10-Icons/8x8 /usr/share/icons/Windows-10-Icons
+sudo cp -r Kali-Windows-10-Icons/16x16 /usr/share/icons/Windows-10-Icons
+sudo cp -r Kali-Windows-10-Icons/22x22 /usr/share/icons/Windows-10-Icons
+sudo cp -r Kali-Windows-10-Icons/24x24 /usr/share/icons/Windows-10-Icons
+sudo cp -r Kali-Windows-10-Icons/32x32 /usr/share/icons/Windows-10-Icons
+sudo cp -r Kali-Windows-10-Icons/48x48 /usr/share/icons/Windows-10-Icons
+sudo cp -r Kali-Windows-10-Icons/128x128 /usr/share/icons/Windows-10-Icons
+sudo cp -r Kali-Windows-10-Icons/256x256 /usr/share/icons/Windows-10-Icons
+sudo cp -r Kali-Windows-10-Icons/512x512 /usr/share/icons/Windows-10-Icons
+sudo cp -r Kali-Windows-10-Icons/cursors /usr/share/icons/Windows-10-Icons
+sudo cp -r Kali-Windows-10-Icons/scalable /usr/share/icons/Windows-10-Icons
+sudo cp -r Kali-Windows-10-Icons/index.theme /usr/share/icons/Windows-10-Icons
+echo "making sure the windows 10 icons can be read but not deleted"
+sudo chmod 555 -R /usr/share/icons/Windows-10-Icons
+echo "All commands finished successfully, your computer should now look like windows 10."
