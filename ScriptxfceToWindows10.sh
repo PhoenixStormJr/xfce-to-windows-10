3146904d617fab42e3e42a5b7a1db7e71fe5beb0
PhoenixStormJr <66498788+PhoenixStormJr@users.noreply.github.com>
2023-07-22 04:17:37 +0000

    Update ScriptxfceToWindows10.sh

--- ScriptxfceToWindows10.sh
+++ ScriptxfceToWindows10.sh
@@ -103,6 +103,7 @@ xfconf-query --channel xfce4-desktop --list | grep last-image | while read path;
 done
 echo "copying shortcuts, which, on linux are known as .desktop files, to their proper places."
 sudo cp -r setupStuff/desktopFiles/applications /usr/share/applications
+sudo cp -r setupStuff/desktopFiles/Desktop /home/$USER/Desktop/
 echo "Now changing mousepad, or notepad settings, to be easier to use"
 sudo apt install dconf-cli
 dconf load /org/xfce/mousepad/ < setupStuff/mousepad.settings
