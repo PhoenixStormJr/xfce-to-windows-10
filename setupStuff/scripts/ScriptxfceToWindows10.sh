9b255a36275f6665330ee99f5c7be0c202c12244
PhoenixStormJr <66498788+PhoenixStormJr@users.noreply.github.com>
2025-07-06 20:31:32 -0500

    Update ScriptxfceToWindows10.sh

--- setupStuff/scripts/ScriptxfceToWindows10.sh
+++ setupStuff/scripts/ScriptxfceToWindows10.sh
@@ -81,6 +81,44 @@ EOF
 fi
 
 
+#Creating Windows 10 Icons:
+ICON_DIR="/usr/share/icons/Windows-10-Icons"
+echo "Checking if Windows 10 icon theme is already installed..."
+if [ ! -d "$ICON_DIR" ]; then
+    echo "Icon theme not found. Proceeding with installation..."
+    echo "Updating package lists..."
+    sudo apt update
+    echo "downloading windows 10 icon theme from b00merang"
+    git clone https://github.com/B00merang-Artwork/Windows-10.git
+    echo "changing name of the icon folder to Windows-10-Icons"
+    mv Windows-10 Windows-10-Icons
+    echo "Moving Windows-10-Icons to /usr/share/icons"
+    sudo mv Windows-10-Icons /usr/share/icons
+    echo "untarring kali windows 10 icons"
+    tar -xvf setupStuff/Kali-Windows-10-Icons.tar
+    echo "copying missing icons to /usr/share/icons/Windows-10-Icons"
+    sudo cp -r Kali-Windows-10-Icons/8x8 /usr/share/icons/Windows-10-Icons
+    sudo cp -r Kali-Windows-10-Icons/16x16 /usr/share/icons/Windows-10-Icons
+    sudo cp -r Kali-Windows-10-Icons/22x22 /usr/share/icons/Windows-10-Icons
+    sudo cp -r Kali-Windows-10-Icons/24x24 /usr/share/icons/Windows-10-Icons
+    sudo cp -r Kali-Windows-10-Icons/32x32 /usr/share/icons/Windows-10-Icons
+    sudo cp -r Kali-Windows-10-Icons/48x48 /usr/share/icons/Windows-10-Icons
+    sudo cp -r Kali-Windows-10-Icons/128x128 /usr/share/icons/Windows-10-Icons
+    sudo cp -r Kali-Windows-10-Icons/256x256 /usr/share/icons/Windows-10-Icons
+    sudo cp -r Kali-Windows-10-Icons/512x512 /usr/share/icons/Windows-10-Icons
+    sudo cp -r Kali-Windows-10-Icons/cursors /usr/share/icons/Windows-10-Icons
+    sudo cp -r Kali-Windows-10-Icons/scalable /usr/share/icons/Windows-10-Icons
+    sudo cp -r Kali-Windows-10-Icons/index.theme /usr/share/icons/Windows-10-Icons
+    echo "multiple icons have not been copied from Windows, I copied some and others I found online."
+    sudo rm /usr/share/icons/Windows-10-Icons/22x22/apps/thunderbird.svg
+    sudo rm /usr/share/icons/Windows-10-Icons/24x24/apps/thunderbird.svg
+    sudo rm /usr/share/icons/Windows-10-Icons/32x32/apps/thunderbird.svg
+    sudo rm /usr/share/icons/Windows-10-Icons/48x48/apps/thunderbird.svg
+    sudo rm /usr/share/icons/Windows-10-Icons/128x128/apps/thunderbird.svg
+    sudo cp -r setupStuff/MissingIcons/Windows-10-Icons /usr/share/icons/
+else
+    echo "Windows 10 icon theme already exists. Skipping installation."
+fi
 
 
 
@@ -170,44 +208,6 @@ else
 fi
 
 
-#Creating an icon directory variable
-ICON_DIR="/usr/share/icons/Windows-10-Icons"
-echo "Checking if Windows 10 icon theme is already installed..."
-if [ ! -d "$ICON_DIR" ]; then
-    echo "Icon theme not found. Proceeding with installation..."
-    echo "Updating package lists..."
-    sudo apt update
-    echo "downloading windows 10 icon theme from b00merang"
-    git clone https://github.com/B00merang-Artwork/Windows-10.git
-    echo "changing name of the icon folder to Windows-10-Icons"
-    mv Windows-10 Windows-10-Icons
-    echo "Moving Windows-10-Icons to /usr/share/icons"
-    sudo mv Windows-10-Icons /usr/share/icons
-    echo "untarring kali windows 10 icons"
-    tar -xvf setupStuff/Kali-Windows-10-Icons.tar
-    echo "copying missing icons to /usr/share/icons/Windows-10-Icons"
-    sudo cp -r Kali-Windows-10-Icons/8x8 /usr/share/icons/Windows-10-Icons
-    sudo cp -r Kali-Windows-10-Icons/16x16 /usr/share/icons/Windows-10-Icons
-    sudo cp -r Kali-Windows-10-Icons/22x22 /usr/share/icons/Windows-10-Icons
-    sudo cp -r Kali-Windows-10-Icons/24x24 /usr/share/icons/Windows-10-Icons
-    sudo cp -r Kali-Windows-10-Icons/32x32 /usr/share/icons/Windows-10-Icons
-    sudo cp -r Kali-Windows-10-Icons/48x48 /usr/share/icons/Windows-10-Icons
-    sudo cp -r Kali-Windows-10-Icons/128x128 /usr/share/icons/Windows-10-Icons
-    sudo cp -r Kali-Windows-10-Icons/256x256 /usr/share/icons/Windows-10-Icons
-    sudo cp -r Kali-Windows-10-Icons/512x512 /usr/share/icons/Windows-10-Icons
-    sudo cp -r Kali-Windows-10-Icons/cursors /usr/share/icons/Windows-10-Icons
-    sudo cp -r Kali-Windows-10-Icons/scalable /usr/share/icons/Windows-10-Icons
-    sudo cp -r Kali-Windows-10-Icons/index.theme /usr/share/icons/Windows-10-Icons
-    echo "multiple icons have not been copied from Windows, I copied some and others I found online."
-    sudo rm /usr/share/icons/Windows-10-Icons/22x22/apps/thunderbird.svg
-    sudo rm /usr/share/icons/Windows-10-Icons/24x24/apps/thunderbird.svg
-    sudo rm /usr/share/icons/Windows-10-Icons/32x32/apps/thunderbird.svg
-    sudo rm /usr/share/icons/Windows-10-Icons/48x48/apps/thunderbird.svg
-    sudo rm /usr/share/icons/Windows-10-Icons/128x128/apps/thunderbird.svg
-    sudo cp -r setupStuff/MissingIcons/Windows-10-Icons /usr/share/icons/
-else
-    echo "Windows 10 icon theme already exists. Skipping installation."
-fi
 if [ -d "$ICON_DIR" ]; then
     echo "making sure the windows 10 icons can be read but not deleted"
     sudo chmod 555 -R /usr/share/icons/Windows-10-Icons
